fed796caa3b49547b0de2d8b05e211ac9b4f132c
Pranav Dhawan <dhawan.pranav8@gmail.com>
2025-03-02 23:32:24 +0530

    title

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,9 +2,9 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'v0 App',
-  description: 'Created with v0',
-  generator: 'v0.dev',
+  title: 'Matrix',
+  description: 'Enter the matrix',
+  generator: '',
 }
 
 export default function RootLayout({
